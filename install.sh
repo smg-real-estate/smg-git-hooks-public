44d78f8761d5de7666e6de946579392ceb975002
Huy Hua <87658627+huyhua-nvg@users.noreply.github.com>
2025-02-11 14:12:00 +0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,12 @@
+HOOK_DIR=$(git config --local core.hooksPath || echo '.git/hooks') && \
+echo "Installing in $HOOK_DIR..." && \
+mkdir -p "$HOOK_DIR"
+
+url = $(curl -s https://api.github.com/repos/smg-real-estate/smg-git-hooks-public/releases/latest \
+| grep "post-checkout" \
+| tail -1 \
+| cut -d : -f 2,3 \
+| tr -d \")
+
+curl -o $HOOK_DIR $url
+chmod +x "$HOOK_DIR/post-checkout"
