8839edf2de84bb41cfefca3ccde5958d6c3844a7
Huy Hua <87658627+huyhua-nvg@users.noreply.github.com>
2025-06-30 13:51:18 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,22 @@
-# Get the hooks path, defaulting to '.githooks' if not set
-HOOK_DIR=$(git config --local core.hooksPath || echo '.githooks') && \
-
-# If hooks path is '.husky/_', change it to just '.husky'
-if [[ "$HOOK_DIR" == ".husky/_" ]]; then
-    HOOK_DIR=".husky"
+# Exit early if not in a git repository
+if ! git rev-parse --git-dir >/dev/null 2>&1; then
+    echo "not in a git repository. skipping installation" >&2
+    exit 0
 fi
 
+PROJECT_ROOT=$(git rev-parse --show-toplevel)
+HOOK_DIR=$(git config --local core.hooksPath)
+
+case "$HOOK_DIR" in
+    ".husky/_") 
+        HOOK_DIR=".husky"
+        ;; # If hooks path is '.husky/_', change it to just '.husky'
+    "") 
+        HOOK_DIR=".git/hooks"
+        ;;
+esac
+
+echo "Project root: $PROJECT_ROOT"
 echo "Installing in $HOOK_DIR..." && \
 mkdir -p "$HOOK_DIR"
 
@@ -13,4 +24,3 @@ url=$(curl -s https://api.github.com/repos/smg-real-estate/smg-git-hooks-public/
 
 curl -L "$url" > "$HOOK_DIR/post-checkout"
 chmod +x "$HOOK_DIR/post-checkout"
-[ "$HOOK_DIR" = ".githooks" ] && git config --local core.hooksPath .githooks || true
